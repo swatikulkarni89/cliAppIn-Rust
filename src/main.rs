b3b3d39a8ec9c2e78aeaf9585155a10a53877407
Swati kulkarni <swatikulkarni89@gmail.com>
2023-03-09 11:36:47 +0530

    formatted file

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,34 @@
-use std::{env::{self, args}, fs,process, fmt::Error };
-
+use std::{
+    env::{self, args},
+    fmt::Error,
+    fs, process,
+};
 
 use std::result;
 
 fn main() {
-    let args: Vec<String>= env :: args().collect();
-    
-let config = Config::new(&args).unwrap_or_else(|err|{
-    println!("error msg{}",err);
-    process::exit(1);
-});
+    let args: Vec<String> = env::args().collect();
 
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("error msg{}", err);
+        process::exit(1);
+    });
 
-    let content= fs::read_to_string(config.filename).expect(" somthing went wrong");
-    println!("{}",content);
+    let content = fs::read_to_string(config.filename).expect(" somthing went wrong");
+    println!("{}", content);
 }
 
-struct Config{
-    query:String,
-    filename:String,
-
+struct Config {
+    query: String,
+    filename: String,
 }
-impl Config
-{
-    fn new(args:&[String])->Result<Config,&str>{
-    if args.len()<3 {
-          return Err("no enough arguments");
-    
-    }
-    let query= args[1].clone();
-    let filename=args[2].clone();
-     Ok(Config {query, filename})
-    
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("no enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
     }
 }
