ab4c23759d075f23347536236d30feb6fd0f8d40
Swati kulkarni <swatikulkarni89@gmail.com>
2023-03-09 10:33:21 +0530

    CLI app in rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use std::{env::{self, args}, fs};
+use std::{env::{self, args}, fs,process, fmt::Error };
+
+
+use std::result;
 
 fn main() {
     let args: Vec<String>= env :: args().collect();
-let config =Config::new(&args);
+    
+let config = Config::new(&args).unwrap_or_else(|err|{
+    println!("error msg{}",err);
+    process::exit(1);
+});
+
 
     let content= fs::read_to_string(config.filename).expect(" somthing went wrong");
     println!("{}",content);
@@ -14,11 +22,15 @@ struct Config{
 
 }
 impl Config
-{fn new(args:&[String])->Config{
-
+{
+    fn new(args:&[String])->Result<Config,&str>{
+    if args.len()<3 {
+          return Err("no enough arguments");
+    
+    }
     let query= args[1].clone();
     let filename=args[2].clone();
-     Config {query, filename}
+     Ok(Config {query, filename})
     
     }
 }
